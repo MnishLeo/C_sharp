12ce7bd6a082b978335ba1538b6d78e1a1c659a3
Manish <manish@outlook.com>
2026-05-07 21:59:21 +0530

    do_while

---
+++ c sharp Basic/Do_While.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace c_sharp_Basic;
+
+class Do_While
+{
+    static void Main()
+    {
+        string userChoice = string.Empty;
+        do
+        {
+            Console.WriteLine("Please Enter your target");
+            int userTarget = int.Parse(Console.ReadLine());
+
+            int start = 0;
+
+            while (start <= userTarget)
+            {
+                Console.Write(start + " ");
+                start = start + 2;
+            }
+            Console.WriteLine(); // Add a new line for better formatting
+
+            do
+            {
+                Console.WriteLine("Do You Want to Continue - Yes or No?");
+                userChoice = Console.ReadLine().ToUpper();
+                if (userChoice != "YES" && userChoice != "NO")
+                {
+                    Console.WriteLine("Invalid Choice. Please say Yes or No");
+                }
+            } while (userChoice != "YES" && userChoice != "NO");
+
+        } while (userChoice == "YES"); // Fixed: Changed from userChoice != "YES" to userChoice == "YES"
+    }
+}
