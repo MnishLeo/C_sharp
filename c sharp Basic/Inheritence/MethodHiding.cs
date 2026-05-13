4d84d4a9114a3ea98edaea519901fcd570adf6bf
Manish <manish@outlook.com>
2026-05-13 21:04:01 +0530

    method-hiding

---
+++ c sharp Basic/Inheritence/MethodHiding.cs
@@ -0,0 +1,39 @@
+﻿namespace run.Inheritence
+{
+    using System;
+
+    public class Employee1
+    {
+        public string firstName;
+        public string lastName;
+
+        public void printFullName()
+        {
+            Console.WriteLine(firstName + " " + lastName);
+        }
+    }
+
+    public class FullTimeEmployee : Employee1
+    {
+        public new void printFullName()
+        {
+            Console.WriteLine(firstName + " " + lastName + " - Contractor");
+        }
+    }
+
+    public class PartTimeEmployee : Employee1
+    {
+        // PartTimeEmployee inherits Employee1's printFullName
+    }
+
+    public class MethodHiding
+    {
+        public static void Main()
+        {
+            FullTimeEmployee employee = new FullTimeEmployee();
+            employee.firstName = "Gog";
+            employee.lastName = "Don";
+            employee.printFullName(); // Outputs: Gog Don - Contractor
+        }
+    }
+}
