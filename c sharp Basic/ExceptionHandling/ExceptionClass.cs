cab9933bb306656b80097bf2752cdfe31f6454b1
Manish <Inozntashu222@gmail.com>
2026-06-04 22:27:49 +0530

    basic exception

---
+++ c sharp Basic/ExceptionHandling/ExceptionClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace c_sharp_Basic.ExceptionHandling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int number = int.Parse("not a number");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Invalid format!");
+            }
+            catch (Exception ex) // General catch
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Always runs");
+            }
+        }
+    }
+}
