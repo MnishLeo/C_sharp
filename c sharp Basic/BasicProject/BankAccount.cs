4b2da06a5997e3c87867aa68c35fe49f5e41dea5
Manish <Inozntashu222@gmail.com>
2026-06-05 21:25:17 +0530

    project-setup

---
+++ c sharp Basic/BasicProject/BankAccount.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Transactions;
+
+namespace c_sharp_Basic.BasicProject
+{
+    public class BankAccount
+    {
+        private String AccountNumber { get; set;}
+        public string AccountHolder { get; set; }
+        public string Pin { get; private set; }
+        public decimal Balance { get; private set; }
+        public List<Transaction> Transactions { get; private set; }
+        public string AccountType { get; set; }
+
+        public BankAccount(string accountHolder , string pin, decimal initialBalance, string accountType)
+        {
+            AccountNumber = GenerateAccountNumber();
+            AccountHolder = accountHolder;
+            Pin = pin;
+            Balance = initialBalance;
+            AccountType = accountType;
+            Transactions = new List<Transaction>();
+        }
+
+
+    }
+}
