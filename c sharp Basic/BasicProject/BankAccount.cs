655a69e631e34c48088c77cc3aea646910b04d5b
Manish <Inozntashu222@gmail.com>
2026-06-08 14:48:49 +0530

    withdraw-deposit

--- c sharp Basic/BasicProject/BankAccount.cs
+++ c sharp Basic/BasicProject/BankAccount.cs
@@ -24,6 +24,41 @@ namespace c_sharp_Basic.BasicProject
             Transactions = new List<Transaction>();
         }
 
+        private String GenerateAccountNumber()
+        {
+            Random random = new Random();
+            return random.Next(10000000, 99999999).ToString();
+        }
 
+        public bool deposit(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Amount must be greater than zero.");
+                return false;
+            }
+            Balance += amount;
+            AddTransaction("Deposit", amount);
+            Console.WriteLine($"Successfully deposited ${amount}. New balance: ${Balance}");
+            return true;
+        }
+
+        public bool withdraw(decimal amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Amount must be greater than zero.");
+                return false;
+            }
+            if (amount > Balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+                return false;
+            }
+            Balance -= amount;
+            AddTransaction("Withdrawal", amount);
+            Console.WriteLine($"Successfully withdrew ${amount}. New balance: ${Balance}");
+            return true;
+        }
     }
 }
